4bf4760cb9b051bdbed499cd0693c93981eb1559
eun-byeol <62530884+eun-byeol@users.noreply.github.com>
2022-12-13 16:21:17 +0900

    feat(SubwayProgram): 지하철 노선 추가 기능 구현

--- src/main/java/subway/domain/SubwayProgram.java
+++ src/main/java/subway/domain/SubwayProgram.java
@@ -2,6 +2,8 @@ package subway.domain;
 
 import subway.utils.ErrorMessage;
 
+import java.util.List;
+
 public class SubwayProgram {
 
     public SubwayProgram() {
@@ -16,6 +18,23 @@ public class SubwayProgram {
     public void deleteStation(String name) {
         if (!StationRepository.deleteStation(name)) {
             throw new IllegalArgumentException(ErrorMessage.NON_EXISTENT_STATION.getDescription());
+        }
+    }
+
+    public void addLine(String LineName, String ascendingTerminusStation, String descendingTerminusStation) {
+        Line newLine = new Line(
+                LineName,
+                new Station(ascendingTerminusStation),
+                new Station(descendingTerminusStation)
+        );
+        LineRepository.addLine(newLine);
+        addStationIfNotExist(ascendingTerminusStation);
+        addStationIfNotExist(descendingTerminusStation);
+    }
+
+    private void addStationIfNotExist(String name) {
+        if (StationRepository.isExistentStation(name)) {
+            addStation(name);
         }
     }
 }
