8cb60439847c90f9d14e8cdbc1f69a9c2da3f43d
eun-byeol <62530884+eun-byeol@users.noreply.github.com>
2022-12-13 18:48:27 +0900

    feat(Application): 명령어 입력부 구현

--- src/main/java/subway/Application.java
+++ src/main/java/subway/Application.java
@@ -1,10 +1,32 @@
 package subway;
 
+import subway.controller.Controller;
+import subway.utils.CommandType;
+
 import java.util.Scanner;
 
 public class Application {
     public static void main(String[] args) {
+        Controller controller = new Controller();
+        boolean isStop = false;
+
+        while(!isStop) {
+            CommandType command = scannerCommand();
+            if (!controller.run(command)) {
+                isStop = true;
+            }
+        }
+    }
+
+    public static CommandType scannerCommand() {
         final Scanner scanner = new Scanner(System.in);
-        // TODO: 프로그램 구현
+        while (true) {
+            try {
+                String command = scanner.next();
+                return CommandType.selectCommandTypeByCode(command);
+            } catch (IllegalArgumentException e) {
+                System.out.println(e.getMessage());
+            }
+        }
     }
 }
