156b53db1a5cd34b9e40ea9f8947df033c5d71f9
eun-byeol <62530884+eun-byeol@users.noreply.github.com>
2022-12-13 16:36:50 +0900

    feat(ErrorMessage): 노선 삭제 오류 메시지 추가

--- src/main/java/subway/utils/ErrorMessage.java
+++ src/main/java/subway/utils/ErrorMessage.java
@@ -4,7 +4,8 @@ public enum ErrorMessage {
     DUPLICATE_NAME("중복된 이름이 존재합니다."),
     NAME_SIZE_OVER_TWO("이름은 2글자 이상이어야 합니다."),
     NON_EXISTENT_STATION("등록되지 않은 역입니다."),
-    STATION_REGISTERED_IN_LINE("노선에 등록된 역은 삭제할 수 없습니다.");
+    STATION_REGISTERED_IN_LINE("노선에 등록된 역은 삭제할 수 없습니다."),
+    NON_EXISTENT_LINE("등록되지 않은 노선입니다.");
 
     private String description;
 
