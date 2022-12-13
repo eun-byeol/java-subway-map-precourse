4720f6adb67d7e4a3f6059d0ae149913edd79f08
eun-byeol <62530884+eun-byeol@users.noreply.github.com>
2022-12-13 17:29:59 +0900

    feat(ErrorMessage): 구간 등록 오류 메시지 추가

--- src/main/java/subway/utils/ErrorMessage.java
+++ src/main/java/subway/utils/ErrorMessage.java
@@ -5,7 +5,11 @@ public enum ErrorMessage {
     NAME_SIZE_OVER_TWO("이름은 2글자 이상이어야 합니다."),
     NON_EXISTENT_STATION("등록되지 않은 역입니다."),
     STATION_REGISTERED_IN_LINE("노선에 등록된 역은 삭제할 수 없습니다."),
-    NON_EXISTENT_LINE("등록되지 않은 노선입니다.");
+    NON_EXISTENT_LINE("등록되지 않은 노선입니다."),
+    EXISTENT_STATION_IN_LINE("이미 등록된 구간입니다."),
+    POSITION_NOT_INTEGER("순서는 정수여야 합니다."),
+    POSITION_HAS_TO_POSITIVE_NUMBER("순서는 1보다 작을 수 없습니다."),
+    POSITION_IS_NOT_VALID_RANGE("순서는 해당 노선의 크기 + 1보다 클 수 없습니다.");
 
     private String description;
 
