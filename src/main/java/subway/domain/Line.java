300afd0d30e5e0aaa07ec18a966781f18de5df98
eun-byeol <62530884+eun-byeol@users.noreply.github.com>
2022-12-13 16:29:07 +0900

    feat(Line): 유효성 검사 메서드 추가

--- src/main/java/subway/domain/Line.java
+++ src/main/java/subway/domain/Line.java
@@ -1,17 +1,37 @@
 package subway.domain;
 
+import subway.utils.ErrorMessage;
+
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.LinkedList;
 
 public class Line {
     private String name;
-    private LinkedList<Station> stations = new LinkedList<>();
+    private LinkedList<Station> stations;
+    private static final int MIN_LENGTH_OF_NAME = 2;
 
-    public Line(String name) {
+    public Line(String name, Station ascending, Station descending) {
+        validateNameSize(name);
+        validateDuplicateName(name);
         this.name = name;
+        stations = new LinkedList<>(Arrays.asList(ascending, descending));
     }
 
     public String getName() {
         return name;
+    }
+
+    private void validateNameSize(String name) {
+        if (name.length() < MIN_LENGTH_OF_NAME) {
+            throw new IllegalArgumentException(ErrorMessage.NAME_SIZE_OVER_TWO.getDescription());
+        }
+    }
+
+    private void validateDuplicateName(String name) {
+        if (LineRepository.isExistentLine(name)) {
+            throw new IllegalArgumentException(ErrorMessage.DUPLICATE_NAME.getDescription());
+        }
     }
 
     public boolean contains (String name) {
