1a968ee05039c01dfbe123f43900fd1b57c8f160
eun-byeol <62530884+eun-byeol@users.noreply.github.com>
2022-12-13 15:57:02 +0900

    feat(LineRepository): 노선에 등록된 역 확인 메서드 구현

--- src/main/java/subway/domain/LineRepository.java
+++ src/main/java/subway/domain/LineRepository.java
@@ -1,15 +1,21 @@
 package subway.domain;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
-import java.util.Objects;
+import java.util.*;
 
 public class LineRepository {
-    private static final List<Line> lines = new ArrayList<>();
+    private static List<Line> lines = new ArrayList<>();
 
     public static List<Line> lines() {
         return Collections.unmodifiableList(lines);
+    }
+
+    public static boolean isInLine(String stationName) {
+        for (Line line : lines) {
+            if (line.contains(stationName)) {
+                return true;
+            }
+        }
+        return false;
     }
 
     public static void addLine(Line line) {
