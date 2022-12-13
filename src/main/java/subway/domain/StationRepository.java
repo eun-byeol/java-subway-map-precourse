c9e1ab2697c5a8cb947bf36a0eb78e08f5dde6f7
eun-byeol <62530884+eun-byeol@users.noreply.github.com>
2022-12-13 15:55:33 +0900

    feat(StationRepository): 역 삭제 예외 처리 구현

--- src/main/java/subway/domain/StationRepository.java
+++ src/main/java/subway/domain/StationRepository.java
@@ -1,5 +1,7 @@
 package subway.domain;
 
+import subway.utils.ErrorMessage;
+
 import java.util.*;
 
 public class StationRepository {
@@ -23,6 +25,13 @@ public class StationRepository {
     }
 
     public static boolean deleteStation(String name) {
+        validateInLine(name);
         return stations.removeIf(station -> Objects.equals(station.getName(), name));
+    }
+
+    private static void validateInLine(String name) {
+        if (LineRepository.isInLine(name)) {
+            throw new IllegalArgumentException(ErrorMessage.STATION_REGISTERED_IN_LINE.getDescription());
+        }
     }
 }
