6537a8f7709769d0eccfbe3951dbb3ba24ccfdbe
eun-byeol <62530884+eun-byeol@users.noreply.github.com>
2022-12-13 18:06:42 +0900

    feat(SubwayProgram): 초기 설정 메서드 추가

--- src/main/java/subway/domain/SubwayProgram.java
+++ src/main/java/subway/domain/SubwayProgram.java
@@ -2,11 +2,31 @@ package subway.domain;
 
 import subway.utils.ErrorMessage;
 
+import java.util.Arrays;
+import java.util.List;
+
 public class SubwayProgram {
 
     public SubwayProgram() {
-        InitialSetting.initialStationRepository();
-        InitialSetting.initialLineRepository();
+        initialStationRepository();
+        initialLineRepository();
+    }
+
+    private void initialStationRepository() {
+        List<String> stationNames = Arrays.asList("교대역, 강남역, 역삼역, 남부터미널역, 양재역, 양재시민의숲역, 매봉역");
+        for (String station : stationNames) {
+            addStation(station);
+        }
+    }
+
+    private void initialLineRepository() {
+        addLine("2호선", "교대역", "역삼역");
+        addLine("3호선", "교대역", "매봉역");
+        addLine("신분당선", "강남역", "양재시민의숲역");
+        addStationInLine("2호선", "강남역", "2");
+        addStationInLine("3호선", "양재역", "2");
+        addStationInLine("3호선", "남부터미널역", "2");
+        addStationInLine("신분당선", "양재역", "2");
     }
 
     public void addStation(String name) {
